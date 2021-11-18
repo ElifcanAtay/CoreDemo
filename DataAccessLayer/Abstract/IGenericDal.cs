d46561a8dddcc1f2ccbad54b1662e399e084ba38
ElifcanAtay <87369681+ElifcanAtay@users.noreply.github.com>
2021-11-18 16:01:54 +0300

    Add files via upload

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T:class
+    {
+        void Inset(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GelListAll();
+        T GetById(int id);
+
+    }
+}
