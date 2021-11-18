c3960b229dbdbad3d89d68d927644999190d8eb7
ElifcanAtay <87369681+ElifcanAtay@users.noreply.github.com>
2021-11-18 16:01:00 +0300

    Add files via upload

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager:ICategoryService
+    {
+        EfCategoryRepository _efCategoryRepository;
+
+        public CategoryManager(EfCategoryRepository efCategoryRepository)
+        {
+            _efCategoryRepository = efCategoryRepository;
+        }
+
+        public void CategoryAdd(Category category)
+        {
+            _efCategoryRepository.Inset(category);
+        }
+
+        public void CategoryDelete(Category category)
+        {
+            _efCategoryRepository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            _efCategoryRepository.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return _efCategoryRepository.GetById(id);
+        }
+
+        public List<Category> GetList()
+        {
+            return _efCategoryRepository.GelListAll();
+        }
+    }
+}
