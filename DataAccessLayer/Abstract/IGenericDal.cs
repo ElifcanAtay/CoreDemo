fbfdd01510d1d7729a6315d8dc446f1b08f10893
ElifcanAtay <87369681+ElifcanAtay@users.noreply.github.com>
2021-12-15 19:49:35 +0300

    40.Ders tamamlandı

--- DataAccessLayer/Abstract/IGenericDal.cs
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,8 +12,8 @@ namespace DataAccessLayer.Abstract
         void Inset(T t);
         void Delete(T t);
         void Update(T t);
-        List<T> GelListAll();
+        List<T>GelListAll();
         T GetById(int id);
-
+        List<T> GetListAllById(Expression<Func<T, bool>> filter);
     }
 }
--- DataAccessLayer/Migrations/20211214103825_mig_newsletter_add.Designer.cs
+++ DataAccessLayer/Migrations/20211214103825_mig_newsletter_add.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20211214103825_mig_newsletter_add")]
+    partial class mig_newsletter_add
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
